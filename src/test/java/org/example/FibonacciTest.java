d6bce2fb8b9d957318bc6750db0d7db46f89ce01
Your Name <yadine.0dz@gmail.com>
2025-03-27 10:37:46 +0100

    exercice 3 , implementing 2 tests pour le moment

---
+++ src/test/java/org/example/FibonacciTest.java
@@ -0,0 +1,19 @@
+package org.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class FibonacciTest {
+    @Test
+    void testFibonacciBaseCases() {
+        assertEquals(0, Fibonacci.fibonacci(0), "Fibonacci(0) should return 0");
+        assertEquals(1, Fibonacci.fibonacci(1), "Fibonacci(1) should return 1");
+    }
+
+    @Test
+    void testFibonnaciSmallValues(){
+        assertEquals(1,Fibonacci.fibonacci(2));
+        assertEquals(2,Fibonacci.fibonacci(3));
+        assertEquals(3,Fibonacci.fibonacci(4));
+        assertEquals(5,Fibonacci.fibonacci(2));
+
+    }
+}
